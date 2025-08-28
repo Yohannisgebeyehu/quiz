cd77dcda73dbc18dc8e358a8e7f82c4a68e807ed
yohannis <gebeyehuyohannis9@gmail.com>
2025-08-28 14:25:46 +0300

    done by yohannis

--- quiz.py
+++ quiz.py
@@ -1,1 +1,26 @@
 
+# Chemistry Questions
+chemistry_questions = [
+    ("Symbol of water?", ["A. CO2", "B. H2O", "C. O2", "D. HO"], "B"),
+("Symbol of sodium?", ["A. Na", "B. So", "C. Sd", "D. S"], "A"),
+    ("Atomic number of Oxygen?", ["A. 6", "B. 7", "C. 8", "D. 9"], "C"),
+    ("Atomic number of Carbon?", ["A. 4", "B. 5", "C. 6", "D. 7"], "C"),
+    ("Formula of carbon dioxide?", ["A. CO2", "B. CO", "C. C2O", "D. C2O2"], "A"),
+    ("Gas needed for breathing?", ["A. CO2", "B. N2", "C. O2", "D. H2"], "C"),
+    ("Which is a noble gas?", ["A. Oxygen", "B. Argon", "C. Nitrogen", "D. Hydrogen"], "B"),
+    ("pH of pure water?", ["A. 0", "B. 7", "C. 14", "D. 10"], "B"),
+    ("Acid in lemon?", ["A. Citric acid", "B. Hydrochloric acid", "C. Sulfuric acid", "D. Lactic acid"], "A"),
+    ("Which acid in stomach?", ["A. HCl", "B. H2SO4", "C. HNO3", "D. CH3COOH"], "A"),
+    ("Table salt formula?", ["A. NaCl", "B. KCl", "C. NaOH", "D. CaCl2"], "A"),
+    ("Atomic number of Hydrogen?", ["A. 0", "B. 1", "C. 2", "D. 3"], "B"),
+    ("Which is a metal?", ["A. Oxygen", "B. Carbon", "C. Iron", "D. Nitrogen"], "C"),
+    ("Which is not a metal?", ["A. Iron", "B. Copper", "C. Gold", "D. Sulfur"], "D"),
+    ("Symbol of gold?", ["A. Ag", "B. Au", "C. Gd", "D. Go"], "B"),
+    ("Symbol of silver?", ["A. Si", "B. S", "C. Ag", "D. Au"], "C"),
+    ("Gas used in balloons?", ["A. Oxygen", "B. Nitrogen", "C. Helium", "D. CO2"], "C"),
+    ("Which acid in vinegar?", ["A. Acetic acid", "B. Citric acid", "C. HCl", "D. Sulfuric acid"], "A"),
+    ("Which element makes bones strong?", ["A. Iron", "B. Calcium", "C. Sodium", "D. Magnesium"], "B"),
+    ("Which metal is liquid at room temperature?", ["A. Gold", "B. Silver", "C. Mercury", "D. Sodium"], "C"),
+]
+#this part is done by yohannis
+
